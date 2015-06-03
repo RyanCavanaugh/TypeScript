881e6c6bc3823b2af6fba29334196739788a32bd
Ryan Cavanaugh <ryanca@microsoft.com>
2015-06-03 11:10:31 -0700

    Update test comments

--- tests/cases/conformance/jsx/tsxAttributeResolution5.jsx
+++ tests/cases/conformance/jsx/tsxAttributeResolution5.jsx
@@ -1,4 +1,11 @@
-function make(obj) {
-    return <test1 {...obj}/>; // should be OK
+function make1(obj) {
+    return <test1 {...obj}/>; // OK
 }
-<test2 {...{}}/>; // should be OK
+function make2(obj) {
+    return <test1 {...obj}/>; // Error (x is number, not string)
+}
+function make3(obj) {
+    return <test1 {...obj}/>; // Error, missing x
+}
+<test1 {...{}}/>; // Error, missing x
+<test2 {...{}}/>; // OK
