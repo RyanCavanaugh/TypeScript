9df6fad422b52d76f9ba3e0638a96d29e18c4608
Ryan Cavanaugh <RyanCavanaugh@users.noreply.github.com>
2016-01-19 13:40:26 -0800

    Add representative testcase

---
+++ tests/baselines/reference/inferredStringIndex.js
@@ -0,0 +1,41 @@
+//// [inferredStringIndex.ts]
+
+interface Headers {
+  [x: string]: string;
+}
+interface Counts {
+  [x: string]: number;
+}
+
+interface Primitives {
+  [x: string]: number|string;
+}
+
+let a = { 'User-agent': 'moz', 'Timeout': undefined }
+let b = { x: 1, y: 2 };
+
+let h: Headers;
+h = a; // OK
+h = b; // Error
+
+let c: Counts;
+c = a; // Error
+c = b; // OK
+
+let p: Primitives
+p = a; // OK
+p = b; // OK
+
+
+//// [inferredStringIndex.js]
+var a = { 'User-agent': 'moz', 'Timeout': undefined };
+var b = { x: 1, y: 2 };
+var h;
+h = a; // OK
+h = b; // Error
+var c;
+c = a; // Error
+c = b; // OK
+var p;
+p = a; // OK
+p = b; // OK
