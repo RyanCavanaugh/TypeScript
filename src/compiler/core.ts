5d3f13eb5fed6a2e1a586f58b9ebcb49d84a0134
Ryan Cavanaugh <RyanCavanaugh@users.noreply.github.com>
2017-12-19 16:19:13 -0800

    Add 'project' option

--- src/compiler/core.ts
+++ src/compiler/core.ts
@@ -2071,6 +2071,13 @@ namespace ts {
                 : moduleKind === ModuleKind.System;
     }
 
+    export function getEmitDeclarations(compilerOptions: CompilerOptions) {
+        if (compilerOptions.project) {
+            return true;
+        }
+        return compilerOptions.declaration;
+    }
+
     export type StrictOptionName = "noImplicitAny" | "noImplicitThis" | "strictNullChecks" | "strictFunctionTypes" | "strictPropertyInitialization" | "alwaysStrict";
 
     export function getStrictOptionValue(compilerOptions: CompilerOptions, flag: StrictOptionName): boolean {
